f74cdb6b84f6ec74c59298b82d34ef9f1e6f546a
Nguyen <hellomytonk123@gmail.com>
2026-05-05 10:27:19 -0700

    added ingredients

---
+++ script.js
@@ -0,0 +1,18 @@
+const ingredients = [
+      { value: "chicken", emoji: "🍗" },
+      { value: "rice", emoji: "🍚" },
+      { value: "cheese", emoji: "🧀" },
+      { value: "eggs", emoji: "🥚" },
+      { value: "pasta", emoji: "🍝" },
+      { value: "tomatoes", emoji: "🍅" },
+      { value: "onion", emoji: "🧅" },
+      { value: "garlic", emoji: "🧄" },
+      { value: "spinach", emoji: "🥬" },
+      { value: "potatoes", emoji: "🥔" },
+      { value: "beef", emoji: "🥩" },
+      { value: "salmon", emoji: "🐟" },
+      { value: "broccoli", emoji: "🥦" },
+      { value: "mushrooms", emoji: "🍄" },
+      { value: "lemon", emoji: "🍋" },
+      { value: "butter", emoji: "🧈" },
+    ];
